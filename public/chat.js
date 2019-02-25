fa834f8c286be90e7583afaa46e3c93c07c22c74
dwiveditanuj41 <tanuj.dwivedi.civ16@itbhu.ac.in>
2019-02-25 20:05:41 +0530

    Message sending on pressing Enter key

--- public/chat.js
+++ public/chat.js
@@ -6,6 +6,40 @@ var output = document.getElementById('output');
 
 
 //emit
+handle.addEventListener('keyup',function(e){
+	if(e.keyCode===13)
+	{if(message.value && handle.value)
+	{
+	socket.emit('chat', {
+		message: message.value,
+		handle: handle.value
+	});}
+
+else
+{
+	noti.innerHTML = '<p>' + 'ERROR! : ' + 'No message or Handle found' + '</p>';
+}}
+
+});
+
+
+message.addEventListener('keyup',function(e){
+	if(e.keyCode===13)
+	{if(message.value && handle.value)
+	{
+	socket.emit('chat', {
+		message: message.value,
+		handle: handle.value
+	});}
+
+else
+{
+	noti.innerHTML = '<p>' + 'ERROR! : ' + 'No message or Handle found' + '</p>';
+}}
+
+});
+
+
 
 btn.addEventListener('click', function(){
 
@@ -29,8 +63,8 @@ socket.on('chat', function(data){
 	noti.innerHTML = '<p>' + '</p>';
 
 	output.innerHTML += '<p>' + '<strong>' + '<span id="sb">'+ data.handle+ '</span>'  + '</strong>' + ': ' + '<span id="ssb">' + data.message + '</span>' + '</p>';
-    message.value=" ";
-    handle.value=" ";
+    message.value="";
+    handle.value="";
 
 });
 
