09258fd638a21d9a3ce47d14347493320dd00887
dwiveditanuj41 <tanuj.dwivedi.civ16@itbhu.ac.in>
2019-02-27 19:58:17 +0530

    database connected - MongoDB working

--- public/chat.js
+++ public/chat.js
@@ -5,7 +5,29 @@ var handle = document.getElementById('handle');
 var btn = document.getElementById('send');
 var output = document.getElementById('output');
 var join = document.getElementById('join');
-var connects = document.getElementById('connects');
+
+socket.on('user',function(data){
+	output.innerHTML +=  '<p style="text-align:center; background-color: white; padding:10px; border: 1px solid white;">'+ '<strong>'+data.handle+'</strong>' + ' joined the chat' + '</p>';
+    output.scrollTop = output.scrollHeight;
+})
+
+
+
+
+socket.on('output', function(data){
+             //console.log(data);
+                    if(data.length){
+                        for(var x = 0;x < data.length;x++){
+                            // Build out message div
+                            
+	                     output.innerHTML += '<div id="bubble"> <p>' + '</strong>'  + '<div id="textmessage" >'+ '<strong>'+ data[x].handle +'</strong><hr />'+ data[x].message + '</div>' + '</p><br /></div>';
+       
+                        }
+                        output.scrollTop = output.scrollHeight;
+                    }
+                    message.value="";
+                });
+
 
 
 // ES6
@@ -37,36 +59,38 @@ else
 });
 
 
-
 join.addEventListener('click', function(){
-
-	socket.emit('user', {
+if(handle.value)
+	{socket.emit('user', {
 		handle: handle.value
-
-	});
+	})};
 });
 
+//Using Enter key
+message.addEventListener('keyup', function(e){
+if(e.keyCode===13)
+	{if(handle.value && message.value)
+	{
+	socket.emit('chat', {
+		handle: handle.value,
+		message: message.value,
+		
+	});}
 
-
-socket.on('user', function(data){
-
-
-
-	output.innerHTML +=  '<p style="text-align:center; background-color: white; padding:10px; border: 1px solid white;">'+ '<strong>'+data.handle+'</strong>' + ' joined the chat' + '</p>';
-    
-
-});
-
-
-
-socket.on('chat', function(data){
-
-	
-
-	output.innerHTML += ' <div id="bubble" > <p>' + '<div id="textmessage" >'+ '<strong>'+ data.handle  +'</strong><hr />'+ data.message + '</div>' + '</p><br /></div>';
+else
+{
+	if(handle.value)
+	{alert("Please enter the message");}
+    else 
+    {
+    	alert("Please enter your Username")
+    }
    
-
+}
+}
 });
+
+
 
 
 
--- server.js
+++ server.js
@@ -3,32 +3,61 @@ var socket = require('socket.io');
 var app = express();
 var http = require('http');
 var server = app.listen(3000, function() {console.log('Server is running on PORT 3000');});
-
-
+const mongo=require('mongodb').MongoClient;
+const client = socket(server);
+var mongoose = require('mongoose');
+//express
 app.use(express.static('public'));
 
-var io = socket(server);
+connections= [];
 
 
+//mongodb connection
 
+mongoose.connect('mongodb://127.0.0.1/chatapp', function(err, db){
+    if(err){
+        throw err;
+    }
 
-io.on('connection', function(socket){
-	
-	socket.on('chat', function(data){
-		connects.push(data.handle);
-	io.sockets.emit('chat', data);
+    console.log('MongoDB connected...');
 
+    // Connect to Socket.io
+    client.on('connection', function(socket){
+        let chat = db.collection('chats');
 
+        
+        // Get chats from mongo collection
+        chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
+            if(err){
+                throw err;
+            }
+
+            // Emit the messages
+            socket.emit('output', res);
+        });
+
+        // Handle input events
+        socket.on('chat', function(data){
+            let handle = data.handle;
+            let message = data.message;
+
+            
+                chat.insert({handle: handle, message: message}, function(){
+                    client.emit('output', [data]);
+
+                    
+                });
+            });
+
+         socket.on('user', function(data){
+            
+            
+
+                    client.emit('user', data);
+
+                    
+                });
+            
+
+        });
 });
-});
-     
-	
-
-	
-	
- 
-
-
-
-
-
